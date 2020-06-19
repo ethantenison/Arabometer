2e571fac6866d856c42858744019a955d569acd1
ethantenison <tenison.ethan@gmail.com>
2020-06-19 17:02:05 -0500

    Created csv and png file

--- mock.py
+++ mock.py
@@ -4,25 +4,33 @@ import matplotlib.image as mpimg
 from PIL import Image, ImageDraw, ImageFilter
 import random
 
+def main(): 
+    try:
+        alarabiya = Image.open('./images/alarabiya.png') 
+        lbc = Image.open('./images/lbc.png') 
+        tele = Image.open('./images/tele_liban.png')
+        future = Image.open('./images/future_tv.png') 
+        aljadeed = Image.open('./images/aljadeed.png') 
+        cover_ala = Image.open('./images/coverphoto_alarabiya.png')
+        blank = Image.open('./images/blank.png')
 
-alarabiya = Image.open('images/alarabiya.png') 
-lbc = Image.open('images/lbc.png') 
-tele = Image.open('images/tele_liban.png')
-future = Image.open('images/future_tv.png') 
-aljadeed = Image.open('images/aljadeed.png') 
-cover_ala = Image.open('images/coverphoto_alarabiya.png')
-blank = Image.open('images/blank.png')
+        list = []
+        list.append(alarabiya)
+        list.append(aljadeed)
+        list.append(future)
+        list.append(lbc)
+        list.append(tele)
 
-list = []
-list.append(alarabiya)
-list.append(aljadeed)
-list.append(future)
-list.append(lbc)
-list.append(tele)
-
-logo_random = random.randint(0,4)
-back_im = blank.copy()
-back_im.paste(list[logo_random], (1000, 50))
-back_im.paste(cover_ala, (100, 50))
-back_im
-
+        logo_random = random.randint(0,4)
+        back_im = blank.copy()
+        back_im.paste(list[logo_random], (1000, 50))
+        back_im.paste(cover_ala, (100, 50))
+        
+  
+        back_im.show()
+        
+    except IOError: 
+        pass
+        
+if __name__ == "__main__": 
+    main() 
